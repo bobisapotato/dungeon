c74cefc9f300610fb1f34f4d695125a64b9b987b
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-03-07 18:16:52 +0000

    Prototype enemy movement.

---
+++ PRCO204-Supine/Assets/Scripts/NPC.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+/* Makes enemies follow and attack the player */
+public class NPC : MonoBehaviour
+{
+
+	public float lookRadius = 10f;
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		// Get the distance to the player
+		float distance = Vector3.Distance(target.position, transform.position);
+
+		// If inside the radius
+		if (distance <= lookRadius)
+		{
+			// Move towards the player
+			agent.SetDestination(target.position);
+			if (distance <= agent.stoppingDistance)
+			{
+				// Attack
+				FaceTarget();
+			}
+		}
+	}
+
+	// Point towards the player
+	void FaceTarget()
+	{
+		Vector3 direction = (target.position - transform.position).normalized;
+		Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, lookRadius);
+	}
+
+}
