bf023900c07de627930cd53adb2b63b8d95f3781
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-03-09 22:23:54 +0000

    Added test scene for slmn.

---
+++ PRCO204-Supine/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        transform.position += move * 5f * Time.deltaTime;
+    }
+}
