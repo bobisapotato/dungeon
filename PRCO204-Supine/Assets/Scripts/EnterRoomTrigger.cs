d1c07e82c102451932d67241f8e76f4a7a08ac0f
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2020-03-19 17:52:06 +0000

    [SUP-16] - Made rooms unlock
    Doors initially open when player collides
    When player enters the room, all doors to that room lock
    When room is cleared (a bool which can be set from other scripts with the setter), any doors that the player already opened reopen, and the other doors unlock, so that player can collide with them to open.

---
+++ PRCO204-Supine/Assets/Scripts/EnterRoomTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterRoomTrigger : MonoBehaviour
+{
+    // Each door has an enter room trigger
+    // This activates when player collides
+    // Sends message up to the doors parent room to say that player is in the room
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            this.GetComponentInParent<Room>().setPlayerInRoom(true);
+        }
+    }
+}
