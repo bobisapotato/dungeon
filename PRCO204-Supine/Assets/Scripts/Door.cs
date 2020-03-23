1beee733e5b8e4d42245526df2f8181d78462bf5
Solomon Cammack <solomon@solomon.eu.com>
2020-03-23 03:46:14 +0000

    [SUP-16] Merge branch 'lockable-rooms' Pull #5

---
+++ PRCO204-Supine/Assets/Scripts/Door.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Each room has at least one door which can be locked and unlocked
+    // Starts closed, opens when player walks in 
+    // public methods can be accessed from other scripts to lock and unlock it again based on objectives
+
+    public Animator animator;
+    public bool open = false;  //  door always starts closed, opens when player collides for the first time
+    public bool locked = false;
+    
+
+
+    private void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+        if (!this.GetComponentInParent<Room>())
+        {
+            Debug.LogError("No parent room found. A door should always be a child of a room");
+        }
+    }
+
+    private void Update()
+    {
+        // For testing purposes - set locked value in anim in the inspector
+        if (locked)
+        {
+            lockDoor();
+        }
+        else
+        {
+            unlockDoor();
+        }
+
+        // For testing purposes - set open value in inspector
+        if(open)
+        {
+            animator.SetBool("Open", true);
+        }
+        else
+        {
+            animator.SetBool("Open", false);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("IsCollidingWSomething");
+
+        if (collision.gameObject.tag == "Player" && !open && !locked)
+        {
+            animator.SetBool("Open", true);
+            open = true;
+        }
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !open && !locked)
+        {
+            animator.SetBool("Open", true);
+            open = true;
+        }
+
+    }
+
+    public void lockDoor()
+    {
+        // Door can be locked from other scripts - doors close and player can't pass through doorway
+        animator.SetBool("Locked", true);
+        locked = true;
+        
+    }
+
+    public void unlockDoor()
+    {
+        // Door unlocks, opens, can be passed through
+        animator.SetBool("Locked", false);
+        locked = false;
+
+    }
+}
---
+++ PRCO204-Supine/Assets/Scripts/Room.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Room : MonoBehaviour
+{
+    // Rooms have at least one door
+    // Multiple rooms make up a level
+    // Rooms have objectives to be met to clear the room
+    // When player enters room for the first time, all doors lock behind them
+    // When room is cleared doors unlock
+
+    // VARIABLES
+    #region
+    public Door[] doors;
+    public bool roomCleared = false;
+    private bool doorsLocked = false;
+    public bool playerInRoom;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!this.GetComponentInChildren<Door>())
+        {
+            Debug.LogError("No doors are attached to this room. Each room requires at least one door child");
+        }
+
+        doors = this.GetComponentsInChildren<Door>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRoom & !roomCleared & !doorsLocked)
+        {
+            lockAllDoors();
+        }
+        else if (playerInRoom & roomCleared & doorsLocked)
+        {
+            unlockAllDoors();
+        }
+    }
+
+    public void setPlayerInRoom(bool playerInRoomInput)
+    {
+        playerInRoom = playerInRoomInput;
+    }
+
+    public void lockAllDoors()
+    {
+        foreach(Door d in doors)
+        {
+            d.lockDoor();
+        }
+
+        doorsLocked = true;
+    }
+
+    public void unlockAllDoors()
+    {
+        foreach (Door d in doors)
+        {
+            d.unlockDoor();
+        }
+
+        doorsLocked = false;
+    }
+}
