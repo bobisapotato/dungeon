4d2608f37663d5689a5a302382f1a8477776853d
Zack Hawkins <44195783+Fich019@users.noreply.github.com>
2020-04-01 16:58:36 +0100

    Enemy Health.
    Enemy dies when hp = 0. Have a chance to drop an item and can take damage.

---
+++ PRCO204-Supine/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    //Variables / unity events
+    [SerializeField] int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            DropItem();
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    //Takes the amount sent to the method away from the enemies health
+
+    //needs hit.transform.SendMessage("TakeDamage", amount); in player attack
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+    }
+    
+
+    public void DropItem()
+    {
+        System.Random rand = new System.Random();
+        int drop = rand.Next(1, 10);
+
+        if (drop == 10)
+        {
+            //Spawn dropped item here.
+        }
+
+    }
+
+
+}
