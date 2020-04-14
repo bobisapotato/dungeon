d0ff03eecb3178e6b31a91b5a4df0435f3e4bb6d
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-04-14 15:27:43 +0100

    [SUP-40] Added temp room code UI

---
+++ PRCO204-Supine/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text roomCodeUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("FindRoomCode", 0.25f);
+    }
+
+    void FindRoomCode() 
+    {
+        roomCodeUI.text = WSConnection.roomCode;
+    }
+}
