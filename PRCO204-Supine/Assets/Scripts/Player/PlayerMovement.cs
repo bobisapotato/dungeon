448e3bba392b831293d6f4c16db394fda21d16a2
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-04-16 14:27:22 +0100

    [SUP-6] Player rotates with right stick

--- PRCO204-Supine/Assets/Scripts/Player/PlayerMovement.cs
+++ PRCO204-Supine/Assets/Scripts/Player/PlayerMovement.cs
@@ -27,6 +27,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Vector3 move;
 
+    [SerializeField]
+    private Vector2 rotY;
+
     private Rigidbody rb;
 
     private PlayerControls controls;
@@ -62,6 +65,12 @@ public class PlayerMovement : MonoBehaviour
         controls.Gameplay.PlayerMoveZ.performed += ctx => z 
         = ctx.ReadValue<float>();
         controls.Gameplay.PlayerMoveZ.canceled += ctx => z = 0f;
+
+
+        controls.Gameplay.PlayerRotY.performed += ctx => rotY
+        = ctx.ReadValue<Vector2>();
+        controls.Gameplay.PlayerRotY.canceled += ctx => rotY = Vector3.zero;
+
 
         controls.Gameplay.PlayerJump.performed += ctx => Jump();
         controls.Gameplay.PlayerCrouch.performed += ctx => Crouch();
@@ -99,9 +108,9 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = false;
         }
 
-        if (move != new Vector3(0f, 0f, 0f)) 
+        if (rotY != Vector2.zero)
         {
-            FaceTarget(transform.position + move);
+            FaceTarget(rotY);
         }
     }
 
@@ -191,12 +200,11 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Point towards the direction of movement.
-    void FaceTarget(Vector3 move)
+    void FaceTarget(Vector2 rot)
     {
-        Vector3 direction = (move - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation
-            (new Vector3(direction.x, 0, direction.z));
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(-rot.x, 0f, -rot.y));
+
         transform.rotation = Quaternion.Slerp(transform.rotation,
-            lookRotation, Time.deltaTime * rotationSpeed);
+                     lookRotation, Time.deltaTime * rotationSpeed);
     }
 }
