64230faccd6242e9aa4e655951106c32717915aa
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-04-21 12:39:37 +0100

    [SUP-29] Enemy attack updated
    Removed attack from movement script to separate enemy attack script.

--- PRCO204-Supine/Assets/Scripts/Enemy/EnemyAttack.cs
+++ PRCO204-Supine/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -2,34 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
 public class EnemyAttack : MonoBehaviour
 {
-    //Variables
-    Collider hitbox;
+    private GameObject player;
 
-    [SerializeField] int damage;
+    private int damage = 10;
 
+    private float attackCoolDown = 0f;
+    private float attackCoolDownTime = 1f;
+    private float attackRadius = 5f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        hitbox = gameObject.GetComponent<CapsuleCollider>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        // Add to attack cooldown.
+        if (attackCoolDown <= attackCoolDownTime)
         {
-            collision.gameObject.SendMessage("TakeDamage", damage);
+            attackCoolDown += Time.deltaTime;
+        }
+
+        // Get the distance to the player.
+        float movementDistance = Vector3.Distance(player.transform.position,
+            transform.position);
+
+        // If inside the radius & attack isn't on cooldown attack.
+        if (movementDistance <= attackRadius && attackCoolDown > attackCoolDownTime)
+        {
+            attackCoolDown = 0f;
+
+            HealthManager.playerHealth.TakeDamage(damage);
+
+            // Debug until we have UI set up for player health.
+            Debug.Log(HealthManager.playerHealth.GetHealth());
         }
     }
 }
