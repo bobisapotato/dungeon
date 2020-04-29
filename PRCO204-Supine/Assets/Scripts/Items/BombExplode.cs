a88c8246cc7265e78d4c7774b197cc978eada568
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-04-29 13:03:55 +0100

    [SUP-39] Changed how bomb prefabs work, added art and sound and particle effects

--- PRCO204-Supine/Assets/Scripts/Items/BombExplode.cs
+++ PRCO204-Supine/Assets/Scripts/Items/BombExplode.cs
@@ -6,8 +6,20 @@ public class BombExplode : MonoBehaviour
 {
     private Collider hitbox;
 
-    [SerializeField] GameObject bombRad;
-    [SerializeField] int damage;
+    [SerializeField] 
+    private GameObject bombRad;
+    [SerializeField]
+    private GameObject explosion;
+    [SerializeField]
+    private GameObject bombModel;
+
+    [SerializeField] 
+    private int damage;
+
+    private Collider[] hitColliders;
+
+    [SerializeField]
+    private float bombRadius = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +27,8 @@ public class BombExplode : MonoBehaviour
         hitbox = bombRad.GetComponent<SphereCollider>();
         hitbox.enabled = false;
         StartCoroutine(DisableCollider());
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Invoke("Explode", 3f);
     }
 
     private IEnumerator DisableCollider()
@@ -30,28 +38,45 @@ public class BombExplode : MonoBehaviour
         hitbox.enabled = true;
     }
 
-    private void OnTriggerStay(Collider other)
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Enemy")
+    //    {
+    //        other.gameObject.SendMessage("TakeDamage", damage);
+    //        hitbox.enabled = false;
+    //    }
+
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        HealthManager.playerHealth.TakeDamage(damage / 2);
+    //        hitbox.enabled = false;
+    //    }
+    //}
+
+    private void Explode()
     {
-        if (other.gameObject.tag == "Enemy")
+        hitColliders = Physics.OverlapSphere(transform.position, bombRadius);
+
+        foreach (Collider hit in hitColliders)
         {
-            other.gameObject.SendMessage("TakeDamage", damage);
-            hitbox.enabled = false;
-            Destroy(gameObject);
+            GameObject go = hit.gameObject;
+
+            if (go.tag == "Enemey")
+            {
+                go.SendMessage("TakeDamage", damage);
+                hitbox.enabled = false;
+            }
+            else if (go.tag == "Player")
+            {
+                HealthManager.playerHealth.TakeDamage(damage / 2);
+                hitbox.enabled = false;
+            }
         }
 
-        if (other.gameObject.tag == "Player")
-        {
-            HealthManager.playerHealth.TakeDamage(damage / 2);
-            hitbox.enabled = false;
-            Destroy(gameObject);
-        }
+        explosion.SetActive(true);
+        bombModel.SetActive(false);
 
-        else
-        {
-            Invoke("Die", 3f);
-            
-        }
-
+        Invoke("Die", 3f);
     }
 
     private void Die()
